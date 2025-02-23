ed3219bcebb62b1c10f5398122a7f0686c1e28d5
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-02-23 17:00:28 +0000

    Add a global exception handler

---
+++ backend/src/main/java/ma/hmzelidrissi/backend/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package ma.hmzelidrissi.backend.exceptions;
+
+
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
+}
