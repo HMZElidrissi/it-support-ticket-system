69e9c96dcb5a3e3ec4599917db674cd63f5c6b85
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-02-25 16:14:46 +0000

    clean up

--- backend/src/main/resources/db/migration/V2__seed.sql
+++ backend/src/main/resources/db/migration/V2__seed.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (ID, NAME, EMAIL, PASSWORD, ROLE) VALUES (1, 'John Doe', 'john.doe@email.com', '$2a$10$MoEZ5uExgeDlSvGonUwBOek7M0uCRLymFtrLmxcRFtNTXFS5Gx2VW', 'EMPLOYEE');
-INSERT INTO USERS (ID, NAME, EMAIL, PASSWORD, ROLE) VALUES (2, 'Jane Doe', 'jane.doe@email.com', '$2a$10$GBlVMRtZ/sJhFVvx8g584.jdjLBDrAN/nclUN/UhTRbdsc8hYqnra', 'IT_SUPPORT');
+INSERT INTO users (NAME, EMAIL, PASSWORD, ROLE) VALUES ('John Doe', 'john.doe@email.com', '$2a$10$MoEZ5uExgeDlSvGonUwBOek7M0uCRLymFtrLmxcRFtNTXFS5Gx2VW', 'EMPLOYEE');
+INSERT INTO USERS (NAME, EMAIL, PASSWORD, ROLE) VALUES ('Jane Doe', 'jane.doe@email.com', '$2a$10$GBlVMRtZ/sJhFVvx8g584.jdjLBDrAN/nclUN/UhTRbdsc8hYqnra', 'IT_SUPPORT');
